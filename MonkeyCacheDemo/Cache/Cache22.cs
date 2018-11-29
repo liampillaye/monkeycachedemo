ec5aa878e96c6a7cec5b9697319027ea72266860
Liam Pillaye <lpillaye@gmail.com>
2018-11-29 10:22:15 +0200

    * .gitignore: Initial Commit
    * MonkeyCacheDemo.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * .monkeycachedemo.db:
    * MainPage.xaml:
    * MainPage.xaml.cs:
    * Cache22.cs:
    * AppDelegate.cs:
    * MonkeyCacheDemo.csproj:
    * MainActivity.cs:
    * Default.png:
    * MonkeyCacheDemo.UI.csproj:
    * Default@2x.png:
    * AboutAssets.txt:
    * MonkeyCacheDemo.iOS.csproj:
    * AssemblyInfo.cs:
    * Default-568h@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * Tabbar.axml:
    * Default-Portrait@2x.png:
    * Toolbar.axml:
    * LaunchScreen.storyboard:
    * icon.png:
    * Icon.png:
    * Resource.designer.cs:
    * AndroidManifest.xml:
    * MonkeyCacheDemo.Android.csproj:
    * Icon.png:
    * Icon.png:
    * Icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon58.png:
    * Icon76.png:
    * Icon60.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon80.png:
    * icon_round.xml:
    * Icon152.png:
    * Icon180.png:
    * Icon167.png:
    * Icon120.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ MonkeyCacheDemo/Cache/Cache22.cs
@@ -0,0 +1,8 @@
+﻿namespace MonkeyCacheDemo.Cache
+{
+    public class Cache22
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ MonkeyCacheDemo/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using MonkeyCache;
+using MonkeyCacheDemo.Cache;
+using Xamarin.Forms;
+using BarrelFile = MonkeyCache.FileStore.Barrel;
+using BarrelLite = MonkeyCache.LiteDB.Barrel;
+using BarrelSQL = MonkeyCache.SQLite.Barrel;
+using System.Data.SqlTypes;
+
+namespace MonkeyCacheDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        #region Privates
+        private IBarrel _sql;
+        private IBarrel _file;
+        private IBarrel _lite;
+        private readonly string _tKey = "cache22";
+        private readonly string _applicationIdLiteDB = "com.refractored.monkeycachetestlite";
+        private readonly string _applicationIdSqlite = "com.refractored.monkeycachetestsql";
+        private readonly string _applicationIdFileStore = "com.refractored.monkeycachetestfile";
+        #endregion
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //SetupCache
+            BarrelLite.ApplicationId = _applicationIdLiteDB;
+            BarrelFile.ApplicationId = _applicationIdFileStore;
+            BarrelSQL.ApplicationId = _applicationIdSqlite;
+            _sql = BarrelSQL.Current;
+            _lite = BarrelLite.Current;
+            _file = BarrelFile.Current;
+
+            //Event Handlers
+            ButtonLoad.Clicked += ButtonLoad_Clicked;
+            ButtonSave.Clicked += ButtonSave_Clicked;
+            ButtonExpired.Clicked += ButtonExpired_Clicked;
+        }
+
+        private IBarrel GetCurrent()
+        {
+            IBarrel current = null;
+
+            if (UseFileStore.IsToggled)
+                current = _file;
+            else if (UseLiteDB.IsToggled)
+                current = _lite;
+            else
+                current = _sql;
+
+            return current;
+        }
+
+        #region EventHandlers
+        private void ButtonSave_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(FirstName.Text) || string.IsNullOrWhiteSpace(LastName.Text))
+            {
+                DisplayAlert("Info", "Please enter a fullname", "OK");
+                return;
+            }
+            var cache22 = new Cache22 { FirstName = FirstName.Text, LastName = LastName.Text };
+            GetCurrent().Add(_tKey, cache22, TimeSpan.FromMinutes(5.0));
+            DisplayAlert(":)", "Saved!", "OK");
+        }
+
+        private void ButtonLoad_Clicked(object sender, EventArgs e)
+        {
+            var cache22 = GetCurrent().Get<Cache22>(_tKey);
+            if (cache22 == null)
+                DisplayAlert(";(", "Cache22", "OK");
+            else
+            {
+                DisplayAlert(":)", $"Hi {cache22.FirstName} {cache22.LastName}", "OK");
+                var expired = GetCurrent().GetExpiration(_tKey);
+            }
+        }
+
+        private void ButtonExpired_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Is Expired?", GetCurrent().IsExpired(_tKey) ? "Yes" : "No", "OK");
+        }
+        #endregion
+    }
+}
